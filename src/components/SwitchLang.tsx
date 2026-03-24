bd62d1fac69d2bf5741eba4d44f5c3464042330d
Astear17 <go548394@gmail.com>
2026-03-24 18:07:25 +0000

    adding Vietnamese localizations, deadass forgot it

--- src/components/SwitchLang.tsx
+++ src/components/SwitchLang.tsx
@@ -6,7 +6,6 @@ import { useRouter } from 'next/router'
 import Link from 'next/link'
 import { useCookies, withCookies } from 'react-cookie'
 
-// https://headlessui.dev/react/menu#integrating-with-next-js
 const CustomLink = ({ href, children, as, locale, ...props }): JSX.Element => {
   return (
     <Link href={href} as={as} locale={locale} {...props}>
