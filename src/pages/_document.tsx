ca9a8ae90a655c50c3dda829a57734b2aa16d786
Astear17 <go548394@gmail.com>
2026-08-13 11:06:38 +0700

    fix: derive og:image domain dynamically from request host

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,12 +1,28 @@
-import Document, { Head, Html, Main, NextScript } from 'next/document'
+import Document, { DocumentContext, Head, Html, Main, NextScript } from 'next/document'
 
 import siteConfig from '../../config/site.config'
 
-class MyDocument extends Document {
+interface MyDocumentProps {
+  ogImage: string
+}
+
+class MyDocument extends Document<MyDocumentProps> {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx)
+
+    const req = ctx.req
+    const host = req?.headers['x-forwarded-host'] || req?.headers.host || process.env.VERCEL_URL || ''
+    const protocol = req?.headers['x-forwarded-proto'] || (host.includes('localhost') ? 'http' : 'https')
+    const siteUrl = process.env.NEXT_PUBLIC_SITE_URL || `${protocol}://${host}`
+    const ogImage = `${siteUrl}/images/branding.png`
+
+    return { ...initialProps, ogImage }
+  }
+
   render() {
+    const { ogImage } = this.props
     const title = siteConfig.title || 'VercelDrive'
     const description = `Browse, preview, and download files from ${title}. A self-hosted OneDrive file browser.`
-    const ogImage = `${siteConfig.domain}/images/branding.png`
 
     return (
       <Html>
